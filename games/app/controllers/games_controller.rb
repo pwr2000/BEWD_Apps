3c5721e26afc02b7d25df79ccfaf74f1d49c82fe
rhoesada <rhoesada@gmail.com>
2013-11-09 01:55:44 -0800

    Initial load of Rails Apps

---
+++ games/app/controllers/games_controller.rb
@@ -0,0 +1,10 @@
+class GamesController < ApplicationController
+  def index
+  end
+  
+  def about
+  end
+
+  def how_it_works
+  end
+end
---
+++ games/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Games::Application.config.secret_key_base = '95f195b0f01490fbeaa85c10244a9fafd572caed24d9a37b9a5e4e8dc40873d53886c810a06d129282c6364dab43b2a0e66f21100f91b1f8f79a9c795b22c110'
